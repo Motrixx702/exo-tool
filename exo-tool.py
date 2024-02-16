1a794cfbbbd63210a712f3b302e69a801f4ceeff
Motrixx702 <150792247+Motrixx702@users.noreply.github.com>
2024-02-16 11:44:57 +0300

    Add files via upload

---
+++ exo-tool.py
@@ -0,0 +1,11 @@
+##################
+import os 
+os.system ("clear")
+v7x = input ("Enter the site's IP address:")
+print (v7x)
+###################
+
+print ("   Successful attack   "   *10000000)
+
+
+
